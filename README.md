14d905a8c70c8b6dce4236acb7d3142ec5aea00a
Jewel Joseph Lijo <jeweljoseph3734@gmail.com>
2025-08-05 23:38:17 +0530

    1.0

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-This is a simple Text To Speech Convertor created using react and CSS.The screenshots of the website is
-given below.
-
-![Screenshot 2025-02-22 174041](https://github.com/user-attachments/assets/307d7aac-f3ff-4dd7-8468-b0edb32f57ab)
-
-![Screenshot 2025-02-22 174119](https://github.com/user-attachments/assets/81a72c28-15a5-412a-9bf3-dbb0e13f4420)
+This is Speech Text Converter.
---
+++ script.js
@@ -0,0 +1,38 @@
+// Text-to-Speech
+const textInput = document.getElementById('text-input');
+
+function Speak(){
+    const text = textInput.value.trim();
+    if (text) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = 'en-US';
+        utterance.volume = 1;
+        utterance.rate = 1;
+        utterance.pitch = 1;
+        speechSynthesis.speak(utterance);
+    }
+}
+
+// Speech-to-Text
+const recognizedText = document.getElementById('recognized-text');
+
+let recognition = new webkitSpeechRecognition() 
+recognition.lang = 'en-US';
+recognition.maxResults = 10;
+recognition.continuous = true;
+recognition.interimResults = true;
+
+recognition.onresult = (event) => {
+    const transcript = event.results[0][0].transcript;
+    recognizedText.textContent = transcript;
+};
+
+// Start recognition when the user clicks the Start Recognition button
+function StartRecognition(){
+    recognition.start();
+}
+
+// Stop recognition when the user clicks the Stop Recognition button
+function StopRecognition(){
+        recognition.stop();
+}
