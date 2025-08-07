f3fe9283c05a7ba118808cb0506a67ac8b26700e
Jewel Joseph Lijo <jeweljoseph3734@gmail.com>
2025-08-07 18:14:47 +0530

    1.0

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function Speak(){
 // Speech-to-Text
 const recognizedText = document.getElementById('recognized-text');
 
-let recognition= new window.webkitSpeechRecognition();
+let recognition= new webkitSpeechRecognition();
 recognition.lang = 'en-US'; 
 recognition.maxResults = 30;
 recognition.continuous = true;
